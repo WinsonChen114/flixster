0ef5999e6842e5828224edf3d038bcc48a8ed9b8
WinsonChen114 <14winsonchen@gmail.com>
2022-06-09 13:43:56 -0400

    Added basic search functionality

--- script.js
+++ script.js
@@ -3,28 +3,50 @@ const searchInputQS = document.querySelector("#search-input")
 const movieGridQS = document.querySelector("#movie-grid")
 const moreButtonQS = document.querySelector("#load-more-movies-btn")
 
-const apiKey = "a19e39d99902fd05af15d47024f06075"
-page = 1
-const base = "https://image.tmdb.org/t/p/w500"
+const apiKey = ""
+let page = 1
+//base url for images
+const base = "https://image.tmdb.org/t/p/w342"
+//Text from search box
+let searchInput = ""
+
+//Checks if usere searches for a movie
+searchAreaQS.addEventListener("submit", handleFormSubmit)
 
 //Gets currently playing movies
-async function loadCurrentlyPlaying(event)
-{
+async function loadCurrentlyPlaying(event) {
     event.preventDefault()
-    let response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key="+apiKey+"&language=en-US&page="+page)
-    console.log("response is: ")
-    console.log(response)
+    let response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=" + apiKey + "&language=en-US&page=" + page)
+    // console.log("response is: ")
+    // console.log(response)
 
     let responseData = await response.json()
-    console.log("responseData is: ")
-    console.log(responseData)
+    // console.log("responseData is: ")
+    // console.log(responseData)
 
     displayResults(responseData.results)
 }
 
+//When searches for a certain movie from the API
+async function searchResults() {
+    searchInput = searchInputQS.value
+    console.log("searchInput is: " + searchInput)
+
+    let response = await fetch("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + searchInput + "&page=" + page + "&include_adult=false")
+    // console.log("response is: ")
+    // console.log(response)
+
+    let responseData = await response.json()
+    // console.log("responseData is: ")
+    // console.log(responseData)
+
+    displayResults(responseData.results)
+
+}
+
 //Display results from an array
-function displayResults(results)
-{
+function displayResults(results) {
+    //Adds each movie in the array
     results.forEach((movie) => {
         movieGridQS.innerHTML += `
         <div class = "movie-card">
@@ -36,6 +58,19 @@ function displayResults(results)
     })
 }
 
+//Executes code when a user searches for a movie
+function handleFormSubmit(event) {
+
+    event.preventDefault()
+    //clears page for new movies
+    if (movieGridQS) {
+        movieGridQS.innerHTML = ''
+    }
+    //reset page number
+    page = 1;
+
+    searchResults()
+}
 
 //Finds currently playing movies when page loads
 window.onload = loadCurrentlyPlaying
