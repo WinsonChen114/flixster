e35d17d0a1d1f78b8e93a35b8d6cfd9426656faf
WinsonChen114 <14winsonchen@gmail.com>
2022-06-09 12:11:52 -0400

    Added basic query selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchAreaQS = document.querySelector("#search-area")
+const searchInputQS = document.querySelecty("#search-input")
+const movieGridQS = document.querySelector("#movie-grid")
+const moreButtonQS = document.querySelector("#load-more-movies-btn")
